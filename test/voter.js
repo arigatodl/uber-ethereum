6a9080d5e6d9c6ed390a9692332179838210e781
Dulguun Batmunkh <dulguunbatmunkh@gmail.com>
2018-03-20 14:03:40 +0900

    Update voter.js

--- test/voter.js
+++ test/voter.js
@@ -4,7 +4,7 @@ const Token = artifacts.require('EIP20.sol');
 const Voting = artifacts.require('Voting.sol');
 const Registry = artifacts.require('Registry.sol');
 
-contract('Registry', (accounts) => {
+contract('Voting', (accounts) => {
     let minStakeAmount;
     let registry;
     let token;
@@ -30,24 +30,11 @@ contract('Registry', (accounts) => {
         it('should allow a new driver to apply', async () => {
             await token.approve(registry.address, 200, { from: driver0 });
             await registry.apply(200, "s", { from: driver0 });
-
-            /*
-            // get the struct in the mapping
-            const result = await registry.listings.call(listing);
-            // check that Application is initialized correctly
-            assert.strictEqual(result[0].gt(0), true, 'challenge time < now');
-            assert.strictEqual(result[1], false, 'whitelisted != false');
-            assert.strictEqual(result[2], applicant, 'owner of application != address that applied');
-            assert.strictEqual(
-            result[3].toString(10),
-            paramConfig.minDeposit.toString(10),
-            'incorrect unstakedDeposit',
-        );*/
         });
 
         it('should not allow an existing driver to apply', async () => {
-            await registry.apply(20, "s", { from: driver0 })
-            await registry.apply(20, "s", { from: driver0 })
+            await token.approve(registry.address, 200, { from: driver0 });
+            await registry.apply(200, "s", { from: driver0 });
         });
 
     });
@@ -57,24 +44,11 @@ contract('Registry', (accounts) => {
         it('should allow a new driver to apply', async () => {
             await token.approve(registry.address, 200, { from: driver0 });
             await registry.apply(200, "s", { from: driver0 });
-
-            /*
-            // get the struct in the mapping
-            const result = await registry.listings.call(listing);
-            // check that Application is initialized correctly
-            assert.strictEqual(result[0].gt(0), true, 'challenge time < now');
-            assert.strictEqual(result[1], false, 'whitelisted != false');
-            assert.strictEqual(result[2], applicant, 'owner of application != address that applied');
-            assert.strictEqual(
-            result[3].toString(10),
-            paramConfig.minDeposit.toString(10),
-            'incorrect unstakedDeposit',
-        );*/
         });
 
         it('should not allow an existing driver to apply', async () => {
-            await registry.apply(20, "s", { from: driver0 })
-            await registry.apply(20, "s", { from: driver0 })
+            await token.approve(registry.address, 200, { from: driver0 });
+            await registry.apply(200, "s", { from: driver0 });
         });
 
     });
